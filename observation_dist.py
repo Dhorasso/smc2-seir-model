c63ea5dfbea0fb78a9f7958dc4eade9185317767
Dhorasso <115734874+Dhorasso@users.noreply.github.com>
2025-01-17 19:30:37 +0000

    Update observation_dist.py

--- observation_dist.py
+++ observation_dist.py
@@ -1,3 +1,11 @@
+################################################################################################################
+# This function contains the different observation distribution
+#  **IMPORTANT NOTE**: (1) Make sure make sure the column name of the obseration is 'obs' or  change 'obs' here by the actual column name
+#                      (2) Make sure make sure the column name of the model state you use to link with observastio is
+#                          'NI' or change it here to have the same name
+##################################################################################################################
+
+
 import numpy as np
 from scipy.stats import poisson, norm, nbinom
 
